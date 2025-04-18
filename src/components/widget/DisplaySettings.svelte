6acd0e8fa2879c06fd79c6ec315e9cada7cac034
Herschel Pawar <pawarherschel@gmail.com>
2025-04-18 17:35:37 +0530

    feat: add animated icons

--- src/components/widget/DisplaySettings.svelte
+++ src/components/widget/DisplaySettings.svelte
@@ -16,7 +16,7 @@ $: if (hue || hue === 0) {
 }
 </script>
 
-<div id="display-setting" class="float-panel float-panel-closed absolute transition-all w-80 right-4 px-4 py-4">
+<div class="float-panel float-panel-closed absolute transition-all w-80 right-4 px-4 py-4" id="display-setting">
     <div class="flex flex-row gap-2 mb-3 items-center justify-between">
         <div class="flex gap-2 font-bold text-lg text-neutral-900 dark:text-neutral-100 transition relative ml-3
             before:w-1 before:h-4 before:rounded-md before:bg-[var(--primary)]
@@ -26,20 +26,20 @@ $: if (hue || hue === 0) {
             <button aria-label="Reset to Default" class="btn-regular w-7 h-7 rounded-md  active:scale-90"
                     class:opacity-0={hue === defaultHue} class:pointer-events-none={hue === defaultHue} on:click={resetHue}>
                 <div class="text-[var(--btn-content)]">
-                    <Icon icon="fa6-solid:arrow-rotate-left" class="text-[0.875rem]"></Icon>
+                    <Icon class="text-[0.875rem]" icon="fa6-solid:arrow-rotate-left"></Icon>
                 </div>
             </button>
         </div>
         <div class="flex gap-1">
-            <div id="hueValue" class="transition bg-[var(--btn-regular-bg)] w-10 h-7 rounded-md flex justify-center
-            font-bold text-sm items-center text-[var(--btn-content)]">
+            <div class="transition bg-[var(--btn-regular-bg)] w-10 h-7 rounded-md flex justify-center
+            font-bold text-sm items-center text-[var(--btn-content)]" id="hueValue">
                 {hue}
             </div>
         </div>
     </div>
     <div class="w-full h-6 px-1 bg-[oklch(0.80_0.10_0)] dark:bg-[oklch(0.70_0.10_0)] rounded select-none">
-        <input aria-label={i18n(I18nKey.themeColor)} type="range" min="0" max="360" bind:value={hue}
-               class="slider" id="colorSlider" step="5" style="width: 100%">
+        <input aria-label={i18n(I18nKey.themeColor)} bind:value={hue} class="slider" id="colorSlider" max="360"
+               min="0" step="5" style="width: 100%" type="range">
     </div>
 </div>
 

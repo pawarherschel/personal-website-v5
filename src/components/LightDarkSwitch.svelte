ba22f59afb22c2d4b3869712be548183c521e6e9
Herschel Pawar <pawarherschel@gmail.com>
2025-04-18 20:02:50 +0530

    feat: added more animations

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -56,18 +56,17 @@ function hidePanel() {
 	panel.classList.add("float-panel-closed");
 }
 </script>
-<!--TODO: Decide if I want to rip out the "auto"-->
 <!-- z-50 make the panel higher than other float panels -->
 <div class="relative z-50" onmouseleave={hidePanel} role="menu" tabindex="-1">
     <button aria-label="Light/Dark Mode" class="relative btn-plain scale-animation rounded-lg h-11 w-11 active:scale-90" id="scheme-switch" onclick={toggleScheme} onmouseenter={showPanel} role="menuitem">
         <div class="absolute" class:opacity-0={mode !== LIGHT_MODE}>
-            <Icon class="text-[1.25rem]" icon="material-symbols:wb-sunny-outline-rounded"></Icon>
+            <Icon class="text-[3rem]" icon="meteocons:clear-day-fill"></Icon>
         </div>
         <div class="absolute" class:opacity-0={mode !== DARK_MODE}>
-            <Icon class="text-[1.25rem]" icon="material-symbols:dark-mode-outline-rounded"></Icon>
+            <Icon class="text-[3rem]" icon="meteocons:starry-night-fill"></Icon>
         </div>
         <div class="absolute" class:opacity-0={mode !== AUTO_MODE}>
-            <Icon class="text-[1.25rem]" icon="material-symbols:radio-button-partial-outline"></Icon>
+            <Icon class="text-[3rem]" icon="meteocons:solar-eclipse-fill"></Icon>
         </div>
     </button>
 
@@ -77,21 +76,21 @@ function hidePanel() {
                     class:current-theme-btn={mode === LIGHT_MODE}
                     onclick={() => switchScheme(LIGHT_MODE)}
             >
-                <Icon class="text-[1.25rem] mr-3" icon="material-symbols:wb-sunny-outline-rounded"></Icon>
+                <Icon class="text-[3rem] mr-3" icon="meteocons:clear-day-fill"></Icon>
                 {i18n(I18nKey.lightMode)}
             </button>
             <button class="flex transition whitespace-nowrap items-center !justify-start w-full btn-plain scale-animation rounded-lg h-9 px-3 font-medium active:scale-95 mb-0.5"
                     class:current-theme-btn={mode === DARK_MODE}
                     onclick={() => switchScheme(DARK_MODE)}
             >
-                <Icon class="text-[1.25rem] mr-3" icon="material-symbols:dark-mode-outline-rounded"></Icon>
+                <Icon class="text-[3rem] mr-3" icon="meteocons:starry-night-fill"></Icon>
                 {i18n(I18nKey.darkMode)}
             </button>
             <button class="flex transition whitespace-nowrap items-center !justify-start w-full btn-plain scale-animation rounded-lg h-9 px-3 font-medium active:scale-95"
                     class:current-theme-btn={mode === AUTO_MODE}
                     onclick={() => switchScheme(AUTO_MODE)}
             >
-                <Icon class="text-[1.25rem] mr-3" icon="material-symbols:radio-button-partial-outline"></Icon>
+                <Icon class="text-[3rem] mr-3" icon="meteocons:solar-eclipse-fill"></Icon>
                 {i18n(I18nKey.systemMode)}
             </button>
         </div>
